4925fbcfcef6dbfaf5fd011a1fd8f17d6a8d5141
Naveed Mirza <naveedmirza@gmail.com>
2020-04-09 12:42:43 +0000

    updated

--- vpc.tf
+++ vpc.tf
@@ -1,57 +1,18 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.33.0"
-  # insert the 12 required variables here
-}
+  source = "terraform-aws-modules/vpc/aws"
 
-##############################################################################
-# Variables File
-#
-# Here is where we store the default values for all the variables used in our
-# Terraform code. If you create a variable with no default, the user will be
-# prompted to enter it (or define it via config file or command line flags.)
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
-  default     = "mirznav"
-}
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-variable "region" {
-  description = "The region where the resources are created."
-  default     = "us-east-1"
-}
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_prefix" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
-}
-
-variable "instance_type" {
-  description = "Specifies the AWS instance type."
-  default     = "t2.micro"
-}
-
-variable "admin_username" {
-  description = "Administrator user name for mysql"
-  default     = "hashicorp"
-}
-
-variable "height" {
-  default     = "400"
-  description = "Image height in pixels."
-}
-
-variable "width" {
-  default     = "600"
-  description = "Image width in pixels."
-}
-
-variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
